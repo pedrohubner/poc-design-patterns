e2a2394e2dd51384a95554e693c86a42f2d62886
pedrohubner <pedronhubner@gmail.com>
2022-04-05 19:38:57 -0300

    First commit.

---
+++ src/main/java/com/pedrohubner/PocDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.pedrohubner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocDesignPatternsApplication.class, args);
+	}
+
+}
