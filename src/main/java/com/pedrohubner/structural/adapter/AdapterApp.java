29ca788fc3adbe11d542106b1852bf1962588c0f
pedrohubner <pedronhubner@gmail.com>
2022-04-07 20:33:25 -0300

    Creating main.

---
+++ src/main/java/com/pedrohubner/structural/adapter/AdapterApp.java
@@ -0,0 +1,10 @@
+package com.pedrohubner.structural.adapter;
+
+import com.pedrohubner.structural.adapter.datatype.Json;
+
+public class AdapterApp {
+    public static void main(String[] args) {
+        new Json("{}")
+        new JsonToXmlAdapter();
+    }
+}
