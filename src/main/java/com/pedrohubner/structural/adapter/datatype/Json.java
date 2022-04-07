1700b80e6d6d11cdbf0587edc612370c8fff1003
pedrohubner <pedronhubner@gmail.com>
2022-04-07 20:32:51 -0300

    Creating models.

---
+++ src/main/java/com/pedrohubner/structural/adapter/datatype/Json.java
@@ -0,0 +1,14 @@
+package com.pedrohubner.structural.adapter.datatype;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Json {
+    private String content;
+}
