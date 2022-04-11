5c1e54dff5b23285390e60f6b401c23a41517f54
pedrohubner <pedronhubner@gmail.com>
2022-04-10 21:54:57 -0300

    Creating adapter main.

--- src/main/java/com/pedrohubner/structural/adapter/AdapterApp.java
+++ src/main/java/com/pedrohubner/structural/adapter/AdapterApp.java
@@ -1,10 +1,10 @@
 package com.pedrohubner.structural.adapter;
 
-import com.pedrohubner.structural.adapter.datatype.Json;
+import lombok.extern.java.Log;
 
+@Log
 public class AdapterApp {
     public static void main(String[] args) {
-        new Json("{}")
-        new JsonToXmlAdapter();
+        log.info("");
     }
 }
